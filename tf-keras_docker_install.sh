e379fd5a75b0b0bcb8c211081868f3a4790dfe7c
Greg Chu <gregory.h.chu@gmail.com>
2017-03-22 14:14:24 -0700

    Update tf-keras_docker_install.sh

--- tf-keras_docker_install.sh
+++ tf-keras_docker_install.sh
@@ -1,8 +1,5 @@
 apt-get update
 apt-get install python-pip python-dev
 pip install --upgrade pip
-apt-get install vim
-pip install tensorflow 
-pip install keras
-pip install h5py
-pip install Pillow
+apt-get install vim tmux
+pip install tensorflow keras h5py Pillow
